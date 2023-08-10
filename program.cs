76f6ccbcc69b49355c1c74aa3f14db0adcee3666
rider <jil.p@simformsolutions.com>
2023-08-10 17:07:16 +0530

    add: register substraction module

--- program.cs
+++ program.cs
@@ -12,6 +12,9 @@ internal class Calculation
 
             Addition addition = new Addition(20, 30);
             Console.WriteLine("Addition : " + calculation.calculateOperation(addition));
+
+            Substraction substraction = new Substraction(30, 20);
+            Console.WriteLine("Substraction : " + calculation.calculateOperation(substraction));
             
             Console.ReadKey();
         }
